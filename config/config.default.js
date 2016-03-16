a77a7ec47f6067ed8fafde4a1fff1f99f844acd5
William Emfinger <waemfinger@gmail.com>
2016-03-16 09:14:51 -0500

    updated to enable svgs for some model elements.  updated meta-model some and added to the 37 node example.

--- config/config.default.js
+++ config/config.default.js
@@ -12,5 +12,9 @@ config.seedProjects.enable = true;
 config.seedProjects.basePaths = ["./src/seeds"]
 config.seedProjects.defaultProject = "GridlabD"
 
+config.client.log.level = 'info'
+
+config.visualization.svgDirs = ["./src/svgs"] 
+
 validateConfig(config);
 module.exports = config;
