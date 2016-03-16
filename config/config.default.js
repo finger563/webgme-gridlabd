6eb5c56958d0f44c0b0b5013129d923f2560b7ac
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-03-15 20:35:17 -0700

    updated config to enable seeds and require authentication. also added properties to the top-level model object

--- config/config.default.js
+++ config/config.default.js
@@ -3,9 +3,14 @@
 var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
-// Add/overwrite any additional settings here
-// config.server.port = 8080;
-// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+// Authentication
+config.authentication.enable = true;
+config.authentication.allowGuests = false;
+
+// Seeds
+config.seedProjects.enable = true;
+config.seedProjects.basePaths = ["./src/seeds"]
+config.seedProjects.defaultProject = "GridlabD"
 
 validateConfig(config);
 module.exports = config;
